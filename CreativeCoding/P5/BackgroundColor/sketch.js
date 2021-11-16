86b28dbd046c3fb760844f16d223df3a0a64b13f
mazrak <90745617+mazrak@users.noreply.github.com>
2021-11-16 14:07:22 -0500

    background color

--- CreativeCoding/P5/BackgroundColor/sketch.js
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -1,5 +1,6 @@
 function setup() {
-
+createCanvas (windowWidth, windowHeight);//this creates the window W/H to be the size of my browser window
+background (237, 123, 222);// red background
 }
 
 function draw() {
