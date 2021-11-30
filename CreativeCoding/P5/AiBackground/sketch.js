547b91debdf88933cd76460637d836e35df8414a
mazrak <90745617+mazrak@users.noreply.github.com>
2021-11-30 14:22:29 -0500

    background

---
+++ CreativeCoding/P5/AiBackground/sketch.js
@@ -0,0 +1,34 @@
+let img;
+
+function preload(){
+img = loadImage('assets/AiBackgroundSingle.png');
+}
+
+function setup() {
+  //runs once
+  createCanvas(720, 720);
+  fill("red");
+  strokeWeight(8);
+}
+
+function draw() {
+  background(220);
+  for (var y = 0; y < 600; y = y + 300) { // loop to create rows in the y direction
+  for (var x = 0; x < 600; x = x + 300) { // loop to create a row of squares in the x direction
+  image (img,0,0);
+
+
+  translate(60, 60);
+
+    quad(x, y,
+    x + 300, y,
+    x + 300, y + 300,
+    x, y + 300);
+
+  }
+
+  }
+
+
+  noLoop();
+}
