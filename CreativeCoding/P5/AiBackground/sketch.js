fa16ba96018f7166947a28c1c1f1db72bc45a7b6
mazrak <90745617+mazrak@users.noreply.github.com>
2021-11-30 14:29:19 -0500

    Update sketch.js

--- CreativeCoding/P5/AiBackground/sketch.js
+++ CreativeCoding/P5/AiBackground/sketch.js
@@ -11,15 +11,15 @@ function setup() {
   strokeWeight(8);
 }
 
+translate(60, 60);
+
 function draw() {
   background(220);
   for (var y = 0; y < 600; y = y + 300) { // loop to create rows in the y direction
   for (var x = 0; x < 600; x = x + 300) { // loop to create a row of squares in the x direction
   image (img,0,0);
 
-
-  translate(60, 60);
-
+  
     quad(x, y,
     x + 300, y,
     x + 300, y + 300,
@@ -28,7 +28,6 @@ function draw() {
   }
 
   }
-
 
   noLoop();
 }
