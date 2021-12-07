422dd8b8cc44f323872b9b52bc26b54a1761106d
mazrak <90745617+mazrak@users.noreply.github.com>
2021-12-07 08:20:28 -0500

    background

--- CreativeCoding/P5/TuningWithVariablesGIF/sketch.js
+++ CreativeCoding/P5/TuningWithVariablesGIF/sketch.js
@@ -1,28 +1,28 @@
 let gif; //for a gif you need to replace "img" with "gif"
 
 function preload (){
-  gif = loadImage ('AiBackground.gif')
+  gif = loadImage ('assets/AiBackground75.gif')
 }
 
 function setup() { //runs once
   createCanvas(windowWidth, windowHeight);
-  fill("red");
-  strokeWeight(1);
 }
 
 function draw() {
 
-var num = 10; //variable for the number of squares in the array
+background (255)
+
+var num = 15; //variable for the number of squares in the array
 var sideLen = windowWidth/num; //variable for the side length of each square
 
   for (var y = 0; y < windowHeight; y = y + sideLen) { // loop to create rows in the y direction
   for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction
-image (gif, x, y)
+image (gif, x, y, windowWidth/num, windowWidth/num)
   }
   }
 
 
-noLoop();
+// noLoop();
 }
 
 function windowResized() {
